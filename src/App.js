af27df28b935f300479605765a549895c877bb91
Gage Thornberry <gagedoug13@gmail.com>
2019-12-15 17:42:17 -0700

    Made a nav bar and some styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
 import './App.css';
+import Main from './components/Main'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Helloooooo</h1>
+      <nav className="navBar">
+        <ul className="ulNavBar">
+          <li>home</li>
+          <li>slone</li>
+          <li>grown</li>
+        </ul>
+      </nav>
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Body from './Body'
+
+export default function Main() {
+    return (
+        <div className="main">
+            <Body />
+        </div>
+    )
+}
