e19798510bd3ef78895c6f0e43252a06601504c0
Gage Thornberry <gagedoug13@gmail.com>
2019-12-21 00:59:51 -0700

    I added a projects button.

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function About() {
     return (
         <div className="about">
-            <h1>About</h1>
+            <h2>From Oklahoma, moved to Colorado</h2>
+            <h1>graduated from Flatiron School.</h1>
+            <h3>Love music and mountain biking</h3>
         </div>
     )
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,9 +5,11 @@ export default function Main() {
     return (
         <div className="welcome">
             <div>
+                <img className="profilePic" src="blackBackgroundPicture.jpg"></img>
                 <h1 className="myName">Hello, my name is Gage Thornberry.</h1>
-                <h2 className="myTitle">I'm a Full-Stack Software Developer.</h2>
-                <button className="welcomeButton">View my stuff</button>
+                <h2 className="myTitle">I'm a Full-Stack Software Engineer.</h2>
+                <button className = "welcomeButton">About Me</button>
+                <button className="welcomeButton">View My Projects</button>
             </div>
         </div>
     )
