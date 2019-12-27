1ba25222d552b863d546bdf2ba21b8e0ccbb61c4
Gage Thornberry <gagedoug13@gmail.com>
2019-12-26 20:22:05 -0700

    Added project screenshots to project section. Restructured and cleaned up project component. Need to make an about page.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,20 @@ import React from 'react'
 export default function Projects() {
     return (
         <div className="projects">
-            <h1>Projects</h1>
+                <div>
+                    My Line
+                    <img className="myLinePic" src="My-Line-Screenshot.png" alt="my-line"></img>
+                </div>
+
+                <div>
+                    Audio Denver
+                    <img className="audioDenverPic" src="Audio-Denver.png"></img>
+                </div>
+                
+                <div>
+                    Red List
+                    <img className="redListPic" src="Red-List.png"></img>
+                </div>
         </div>
     )
 }
