d5217364dca891dffee789366dd4ccbeaf4471ce
Gage Thornberry <gagedoug13@gmail.com>
2020-04-05 19:12:29 -0600

    Revised about me and am adding css

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function About() {
     return (
         <div className="about">
-            <h2 className='firstAboutPage'>Hello! My name is Gage Thornberry and i'm a full-stack web developer from Oklahoma. I loved playing instruments as a kid, and when I got a little older I learned to make my own music with professional audio software. Seeing the endless potential in useful software inspired me to learn more about computer programming. I attended and graduated from Flatiron School's 15 week program for Software Engineering. </h2>
-            <h2 className='secondAboutParagraph'>During my time in school I used various languages to make a useful applications. One of the best things about going to school to learn computer programming, in my opinion, was the exposure I got to working with other engineers. It's important to know that you can always learn something from the person that you're working with. Now I'm looking for an apportunity to bring value to the next project. </h2>
+            <h2 className='firstAboutPage'>Hello there! My name is Gage Thornberry and I'm a full-stack web developer and musician from Oklahoma. I loved playing instruments as a kid, and when I got a little older I started making my own music with professional audio software. Seeing the endless potential in useful software inspired me to learn more about computer programming. I attended and graduated from Flatiron School's 15 week program for Software Engineering. </h2>
+            <h2 className='secondAboutParagraph'>During my time in school I learned various skills to make problem solving applications using frameworks like React, Ruby on Rails, and Node.js. One of my biggest take-aways from the experience was my exposure to building projects with other people. It's important to know that you can always learn something from the person that you're working with. </h2>
         </div>
     )
 }
