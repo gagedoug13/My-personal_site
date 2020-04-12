ad681e7f3d8016402b321d8613bf5ff6deb01649
Gage Thornberry <gagedoug13@gmail.com>
2020-04-12 17:48:50 -0600

    fixing contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,12 @@ export default function Contact() {
     return (
         <div className="contact">
             <h1>Contact</h1>
+            <div className='contactInfoDiv'>
+                <h1>Phone: (405)708-2614</h1>
+                <h2>Email: gagedoug13@gmail.com</h2>
+                <h2>Github: gagedoug13</h2>
+            </div>
+            
         </div>
     )
 }
