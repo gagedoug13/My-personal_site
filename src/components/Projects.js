e0517a1c2ab2aac265661e4eeeca7ac63d8f7a3f
Gage Thornberry <gagedoug13@gmail.com>
2020-04-12 17:41:42 -0600

    Fixing up projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,19 +4,19 @@ export default function Projects() {
     return (
         <div className="projects">
                 <div className='jamzDiv'>
-                    <h1>
+                    <h1 className='projectText'>
                         Neighborhood Jamz - (picture)
                     </h1>
                 </div>
 
                 <div className='lineDiv'>
-                    <h1>
+                    <h1 className='projectText'>
                         My Line (need to change the name of my line and do media queries)
                     </h1>
                 </div>
 
                 <div className='audioDenverDiv'>
-                    <h1>
+                    <h1 className='projectText'>
                         Audio Denver? Might change if I use this.
                     </h1>
                 </div>
