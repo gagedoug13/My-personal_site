d7cf730973a12eb3f88f685871c332da6c5c3ac9
Gage Thornberry <gagedoug13@gmail.com>
2020-04-12 17:36:37 -0600

    changing backgrounds and stying

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,19 +3,19 @@ import React from 'react'
 export default function Projects() {
     return (
         <div className="projects">
-                <div>
+                <div className='jamzDiv'>
                     <h1>
                         Neighborhood Jamz - (picture)
                     </h1>
                 </div>
 
-                <div>
+                <div className='lineDiv'>
                     <h1>
                         My Line (need to change the name of my line and do media queries)
                     </h1>
                 </div>
 
-                <div>
+                <div className='audioDenverDiv'>
                     <h1>
                         Audio Denver? Might change if I use this.
                     </h1>
