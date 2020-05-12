25c73ea5c3a95e70b1a8d4960148170ab067ed23
Gage Thornberry <gagedoug13@gmail.com>
2020-05-12 15:59:05 -0600

    fixed contact div

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,10 +5,10 @@ export default function Contact() {
         <div className="contact">
             <h1>Contact</h1>
             <div className='contactInfoDiv'>
-                <h1>Phone: (405)708-2614</h1>
-                <h2>Email: gagedoug13@gmail.com</h2>
-                <a href='https://github.com/gagedoug13'><h2>Github: gagedoug13</h2></a>
-                <a href='https://www.linkedin.com/in/gage-thornberry-a32a9b168/'><h2>LinkedIn: gagedoug13</h2></a>
+                <h1 className='contactText'>Phone: (405)708-2614</h1>
+                <h2 className='contactText'>Email: gagedoug13@gmail.com</h2>
+                <a href='https://github.com/gagedoug13'><h2 className='contactText'>Github: gagedoug13</h2></a>
+                <a href='https://www.linkedin.com/in/gage-thornberry-a32a9b168/'><h2 className='contactText'>LinkedIn: gagedoug13</h2></a>
             </div>
         </div>
     )
