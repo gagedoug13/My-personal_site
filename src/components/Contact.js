4548d51488b563daa742da2383393f94cd30506f
Gage Thornberry <gagedoug13@gmail.com>
2020-06-07 14:30:28 -0600

    styling contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,19 +5,19 @@ export default function Contact() {
         <div className="contact">
             <h1>Contact</h1>
             <div className='contactInfoDiv'>
-                <div>
+                <div className='contactBackground'>
                     <h1 className='contactText'>Phone: (405)708-2614</h1>
                 </div>
 
-                <div>
+                <div className='contactBackground'>
                     <h2 className='contactText'>Email: gagedoug13@gmail.com</h2>
                 </div>
 
-                <div>
+                <div className='contactBackground'>
                     <a href='https://github.com/gagedoug13'><h2 className='contactText'>Github: gagedoug13</h2></a>
                 </div>
 
-                <div>
+                <div className='contactBackground'>
                     <a href='https://www.linkedin.com/in/gage-thornberry-a32a9b168/'><h2 className='contactText'>LinkedIn: gagedoug13</h2></a>
                 </div>
             </div>
