a0fa948b37f270505ea9e96e182fc4cb4c16b6db
Gage Thornberry <gagedoug13@gmail.com>
2020-06-07 14:19:24 -0600

    structuring contact page to look nice

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,10 +5,21 @@ export default function Contact() {
         <div className="contact">
             <h1>Contact</h1>
             <div className='contactInfoDiv'>
-                <h1 className='contactText'>Phone: (405)708-2614</h1>
-                <h2 className='contactText'>Email: gagedoug13@gmail.com</h2>
-                <a href='https://github.com/gagedoug13'><h2 className='contactText'>Github: gagedoug13</h2></a>
-                <a href='https://www.linkedin.com/in/gage-thornberry-a32a9b168/'><h2 className='contactText'>LinkedIn: gagedoug13</h2></a>
+                <div>
+                    <h1 className='contactText'>Phone: (405)708-2614</h1>
+                </div>
+
+                <div>
+                    <h2 className='contactText'>Email: gagedoug13@gmail.com</h2>
+                </div>
+
+                <div>
+                    <a href='https://github.com/gagedoug13'><h2 className='contactText'>Github: gagedoug13</h2></a>
+                </div>
+
+                <div>
+                    <a href='https://www.linkedin.com/in/gage-thornberry-a32a9b168/'><h2 className='contactText'>LinkedIn: gagedoug13</h2></a>
+                </div>
             </div>
         </div>
     )
