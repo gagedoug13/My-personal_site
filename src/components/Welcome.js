5c6c97bc1b97a27505ce00c902272836c8928837
Gage Thornberry <gagedoug13@gmail.com>
2020-08-16 12:04:38 -0600

    cleaning code

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -9,11 +9,9 @@ export default function Main() {
                 <h1 className="myName">Hello, my name is Gage Thornberry.</h1>
                 <h2 className="myTitle">I'm a Full-Stack Web Developer and Musician.</h2>
                 <div className='welcomeButtonDiv'>
-                   
                     <button className='welcomeButtons'>Projects</button>
                     <button className='welcomeButtons'>Resume</button>
                     <button className='welcomeButtons'>Contact</button>
-        
                </div>
             </div>
         </div>
